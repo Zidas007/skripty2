e5df64a5d703d06e7041c8cf45cb97d1345c4458
Franta <Franta@seznam.cz>
2017-03-06 07:48:14 +0100

    Verzia pondeli

--- dohled.sh
+++ dohled.sh
@@ -9,14 +9,14 @@ neco=`df -h | grep sda1 | awk -F " " '{print $3}'`
 neco1=`df -h | grep sda1 | awk -F " " '{print $4}'`
 neco2=`df -h | grep sda1 | awk -F " " '{print $5}'`
 
-echo "Aktuální čas: "$cas >> /home/student/Plocha/skripty2/zidek.html
-echo "Moje IP: "$ip >> /home/student/Plocha/skripty2/zidek.html
-echo "Přijatá data: "$rx >> /home/student/Plocha/skripty2/zidek.html
-echo "Odeslaná data: "$tx >> /home/student/Plocha/skripty2/zidek.html
-echo "Lidi na počítači:"$lidi >> /home/student/Plocha/skripty2/zidek.html
-echo "Velikost disku:"$disk >> /home/student/Plocha/skripty2/zidek.html
-echo "Užito disku:"$neco >> /home/student/Plocha/skripty2/zidek.html
-echo "Volné místo na disku:"$neco1 >> /home/student/Plocha/skripty2/zidek.html
-echo "Procento využití disku:"$neco2 >> /home/student/Plocha/skripty2/zidek.html
-
+echo "Aktualni cas: "$cas >> /home/student/Plocha/skripty2/index.html
+echo "Moje IP: "$ip >> /home/student/Plocha/skripty2/index.html
+echo "Prijata data: "$rx >> /home/student/Plocha/skripty2/index.html
+echo "Odeslana data: "$tx >> /home/student/Plocha/skripty2/index.html
+echo "Lidi na pocitaci:"$lidi >> /home/student/Plocha/skripty2/index.html
+echo "Velikost disku:"$disk >> /home/student/Plocha/skripty2/index.html
+echo "Uzito disku:"$neco >> /home/student/Plocha/skripty2/index.html
+echo "Volne misto na disku:"$neco1 >> /home/student/Plocha/skripty2/index.html
+echo "Procento vyuziti disku:"$neco2 >> /home/student/Plocha/skripty2/index.html
+echo "<br><br>" >> /home/student/Plocha/skripty2/index.html
 
